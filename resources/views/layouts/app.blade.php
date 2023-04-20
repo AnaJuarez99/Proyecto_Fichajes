45acc0b699b87d579e9811e730619bbf39a483c6
Francisco <practicas@solucionesinformaticas>
2023-04-20 10:06:00 +0200

    Cambio de iconos nav
    Cambiamos los iconos del nav por font awesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,7 @@
   <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
   <!-- summernote -->
   <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
+
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
@@ -166,7 +167,7 @@
                with font-awesome or any other icon font library -->
                <li class="nav-item">
                 <a href="{{ route('inicio') }}" class="nav-link">
-                  <i class="nav-icon fas fa-th"></i>
+                  <i class="nav-icon fas fa-home"></i>
                   <p>
                     Inicio
                     <!-- <span class="right badge badge-danger">New</span> -->
@@ -176,7 +177,7 @@
 
                <li class="nav-item">
                 <a href="{{ route('historial') }}" class="nav-link">
-                  <i class="nav-icon fas fa-th"></i>
+                  <i class="nav-icon fas fa-history"></i>
                   <p>
                     Historial
                      <!--<span class="right badge badge-danger">New</span> -->
