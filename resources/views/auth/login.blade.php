80468903b200fec11bc84ff3532d61eb9a304b3b
Ana Juarez <practicas@solucionesinformaticasmj.com>
2023-05-04 09:20:49 +0200

    Cambios estilos Login y Registro
    Cambios estilos Login y Registro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,13 +5,14 @@
     <div class="row justify-content-center align-items-center min-vh-100">
         <div class="col-lg-6 col-sm-12 ">
             <div class="bg-light p-5 rounded ">
-                <h1 class="font-weight-bold mb-5 text-center fs-70">Login</h1>
+                <div class="card-header text-center h3-custom   fs-65">{{ __('LOGIN') }}</div>
+
 
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
 
                     <div class="form-group">
-                        <label for="email" class="font-weight-medium fs-50">Correo electrónico</label>
+                        <label for="email" class="fs-22 font-weight-normal fs-50">Correo electrónico</label>
                         <input id="email" type="email" class="inputs2 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Introduzca el correo electrónico">
 
                         @error('email')
@@ -22,7 +23,7 @@
                     </div>
 
                     <div class="form-group">
-                        <label for="password" class="font-weight-medium fs-50">Contraseña</label>
+                        <label for="password" class="fs-22 font-weight-normal fs-50">Contraseña</label>
                         <input id="password" type="password" class="inputs2  @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Introduzca la contraseña">
 
                         @error('password')
@@ -42,9 +43,9 @@
                         </div>
                     </div> --}}
 
-                    <div class="form-group text-right">
+                    <div class="form-group text-right fs-27">
                         @if (Route::has('password.request'))
-                            <a class=" btn-link p-0 text-decoration-none fs-30" href="{{ route('password.request') }}">
+                            <a class=" btn-link p-0 text-decoration-none forgotpass" href="{{ route('password.request') }}">
                                 {{ __('Forgot Your Password?') }}
                             </a>
                         @endif
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center ">
-        <div class="col-lg-6 col-sm-12">
+        <div class="col-lg-6 col-sm-12 registro">
             <div class="card rounded-2 ">
-                <div class="card-header text-center h3-custom  bg-E5E7E9 fs-70">{{ __('REGISTRO') }}</div>
+                <div class="card-header text-center h3-custom   fs-70">{{ __('REGISTRO') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -15,7 +15,7 @@
                             <label for="nombre" class="fs-22 font-weight-normal fs-50">{{ __('Nombre') }}</label>
 
                             <div class="w-100">
-                                <input id="nombre" type="text" class="fs-18 bg-E5E7E9 inputs2 @error('nombre') is-invalid @enderror" name="nombre" value="{{ old('nombre') }}" required autocomplete="nombre" autofocus>
+                                <input id="nombre" type="text" class="fs-12 bg-E5E7E9 inputs2 @error('nombre') is-invalid @enderror" name="nombre" value="{{ old('nombre') }}" required autocomplete="nombre" autofocus>
 
                                 @error('nombre')
                                     <span class="invalid-feedback" role="alert">
@@ -29,7 +29,7 @@
                             <label for="apellidos" class="fs-22 font-weight-normal fs-50">{{ __('Apellidos') }}</label>
 
                             <div class="w-100">
-                                <input id="apellidos" type="text" class="fs-18 bg-E5E7E9 inputs2 @error('apellidos') is-invalid @enderror" name="apellidos" value="{{ old('apellidos') }}" required autocomplete="apellidos" autofocus>
+                                <input id="apellidos" type="text" class="fs-12 bg-E5E7E9 inputs2 @error('apellidos') is-invalid @enderror" name="apellidos" value="{{ old('apellidos') }}" required autocomplete="apellidos" autofocus>
 
                                 @error('apellidos')
                                     <span class="invalid-feedback" role="alert">
@@ -43,7 +43,7 @@
                             <label for="dni" class="fs-22 font-weight-normal fs-50">{{ __('DNI') }}</label>
 
                             <div class="w-100">
-                                <input id="dni" type="text" class="fs-18 bg-E5E7E9 inputs2 @error('dni') is-invalid @enderror" name="dni" value="{{ old('dni') }}" required autocomplete="dni" autofocus>
+                                <input id="dni" type="text" class="fs-12 bg-E5E7E9 inputs2 @error('dni') is-invalid @enderror" name="dni" value="{{ old('dni') }}" required autocomplete="dni" autofocus>
 
                                 @error('dni')
                                     <span class="invalid-feedback" role="alert">
@@ -57,7 +57,7 @@
                             <label for="telefono" class="fs-22 font-weight-normal fs-50">{{ __('Teléfono') }}</label>
 
                             <div class="w-100">
-                                <input id="telefono" type="number" class="fs-18 bg-E5E7E9 inputs2 @error('telefono') is-invalid @enderror" name="telefono" value="{{ old('telefono') }}" required autocomplete="telefono" autofocus>
+                                <input id="telefono" type="number" class="fs-12 bg-E5E7E9 inputs2 @error('telefono') is-invalid @enderror" name="telefono" value="{{ old('telefono') }}" required autocomplete="telefono" autofocus>
 
                                 @error('telefono')
                                     <span class="invalid-feedback" role="alert">
@@ -71,7 +71,7 @@
                             <label for="puesto" class="fs-22 font-weight-normal fs-50">{{ __('Puesto de trabajo') }}</label>
 
                             <div class="w-100">
-                                <input id="puesto" type="text font-weight-normal" class="fs-18 bg-E5E7E9 inputs2 @error('puesto') is-invalid @enderror" name="puesto" value="{{ old('puesto') }}" required autocomplete="puesto" autofocus>
+                                <input id="puesto" type="text font-weight-normal" class="fs-12 bg-E5E7E9 inputs2 @error('puesto') is-invalid @enderror" name="puesto" value="{{ old('puesto') }}" required autocomplete="puesto" autofocus>
 
                                 @error('puesto')
                                     <span class="invalid-feedback" role="alert">
@@ -85,7 +85,7 @@
                             <label for="email" class="fs-22 font-weight-normal fs-50">{{ __('Correo electrónico') }}</label>
 
                             <div class="w-100">
-                                <input id="email" type="email" class="fs-18 bg-E5E7E9 inputs2 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" class="fs-12 bg-E5E7E9 inputs2 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -99,7 +99,7 @@
                             <label for="password" class="fs-22 font-weight-normal fs-50">{{ __('Contraseña') }}</label>
 
                             <div class="w-100">
-                                <input id="password" type="password" class="fs-18 bg-E5E7E9 inputs2 @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="fs-12 bg-E5E7E9 inputs2 @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -113,7 +113,7 @@
                             <label for="password-confirm" class="fs-22 font-weight-normal fs-50">{{ __('Confirmar Contraseña') }}</label>
 
                             <div class="w-100">
-                                <input id="password-confirm" type="password" class="fs-18 bg-E5E7E9 inputs2" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="fs-12 bg-E5E7E9 inputs2" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
